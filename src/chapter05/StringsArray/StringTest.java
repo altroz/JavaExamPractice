ea49e1ac3b75fdf104a14668a64bcf2ae77e79b4
altroz <nhb913@outlook.com>
2015-06-19 15:00:52 -0700

    Three ways of initiating String object. 1. String s = new String(); s = "abc"; 2. String s = new String("abc"); 3. String s = "abc";

---
+++ src/chapter05/StringsArray/StringTest.java
@@ -0,0 +1,14 @@
+package chapter05.StringsArray;
+
+public class StringTest {
+	public static void main(String[] args){
+	String a = new String();
+	a = "abc";
+	
+	String b = new String("abc");
+	String c = "abc";
+	
+	
+	}
+
+}
