9997f9bda5e4ce95f288c30fa2980a904a982d80
altroz <nhb913@outlook.com>
2015-06-19 15:27:44 -0700

    Both are revision of the StringTest1.java

---
+++ src/chapter05/StringBuffer/StringTest1.java
@@ -0,0 +1,24 @@
+package chapter05.StringBuffer;
+
+public class StringTest1 {
+	public static void main(String[] args){
+		String a = "abc";
+		// Creates one String object and one reference variable.
+		
+		String b = new String("cfg");
+		/*Creates two string object and one reference variable. 
+		 * 1. The new String object resides in memory. 
+		 * 2. The literal "abc" is placed in the pool*/
+		
+		// Other methods for String
+		a.charAt(2);
+		a.concat(b);
+		a.equals(b);
+		a.length();
+		a.replace("a","q");
+		a.substring(1, 2);
+		
+		
+		
+	}
+}		
