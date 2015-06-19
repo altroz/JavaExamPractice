e8b257b087963b67b5a4941c384c49476cd3a38d
altroz <nhb913@outlook.com>
2015-06-18 21:13:15 -0700

    Invoking a Superclass version of an overridden method

---
+++ src/chapter02/Polymorphism2/Porsche.java
@@ -0,0 +1,7 @@
+package chapter02.Polymorphism2;
+
+public class Porsche extends Car{
+	public void changeGear(){
+		super.changeGear();
+	}
+}
---
+++ src/chapter02/Polymorphism2/TestCar.java
@@ -0,0 +1,8 @@
+package chapter02.Polymorphism2;
+
+public class TestCar {
+	public static void main(String [] args){
+		Car a = new Porsche();
+		a.changeGear();
+	}
+}
