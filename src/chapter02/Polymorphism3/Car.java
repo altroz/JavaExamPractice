115df6b0b7ec6796b02a7cf2b5585b8a236af74b
altroz <nhb913@outlook.com>
2015-06-18 21:19:35 -0700

    There is a compiling error as the method drive(); being overridden is not throwing an exception. The method drive(); is inherited from the Car superclass and hence needed to throw an exception.

---
+++ src/chapter02/Polymorphism3/Car.java
@@ -0,0 +1,7 @@
+package chapter02.Polymorphism3;
+
+public class Car {
+	public void drive() throws Exception{
+		// throw Exception codes
+	}
+}
---
+++ src/chapter02/Polymorphism3/Car2.java
@@ -0,0 +1,12 @@
+package chapter02.Polymorphism3;
+
+public class Car2 extends Car{
+	public void drive(){/* no Exception thrown*/};
+	public static void main(){
+		Car a = new Car2();
+		Car2 b = new Car2();
+		
+		b.drive();
+		a.drive();
+	}
+}
