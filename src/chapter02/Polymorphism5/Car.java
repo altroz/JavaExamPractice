847e38aab6519ed94ea72e203b66a56c1ce43a64
altroz <nhb913@outlook.com>
2015-06-18 21:39:56 -0700

    The following code illustrates invoking overloaded method from the public static void main(String[] args) method. The call to
    public void drive(String destination); is invoked by:
    
    public void drive("Central Union Station");

---
+++ src/chapter02/Polymorphism5/Car.java
@@ -0,0 +1,5 @@
+package chapter02.Polymorphism5;
+
+public class Car {
+	public void drive(){}
+}
---
+++ src/chapter02/Polymorphism5/Ferrari.java
@@ -0,0 +1,7 @@
+package chapter02.Polymorphism5;
+
+public class Ferrari extends Car{
+	public void drive(String destination){}
+	public void drive(String destination, int timeToTake){}
+	public void drive(String destination, String roadToTake){}
+}
