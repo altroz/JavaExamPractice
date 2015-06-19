6994a1cfe24736b119251af4f4954649e9a69898
altroz <nhb913@outlook.com>
2015-06-19 16:15:07 -0700

    Programmatically Thrown Exception.

---
+++ src/chapter08/ExceptionHandling1/ThrowExceptionTest.java
@@ -0,0 +1,15 @@
+package chapter08.ExceptionHandling1;
+
+public class ThrowExceptionTest {
+	public static void main(String[] args){
+		String s = "abc";
+/*		int parseInt(String s1) throws NumberFormatException{
+			boolean parseSuccer = false;
+			int result = 0;
+			// do complicated parsing
+			
+			if(!parseSuccess)
+				throw new NumberFormatException();
+			return result;					*/
+		}
+	}
