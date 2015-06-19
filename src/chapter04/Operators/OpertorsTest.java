f2ceae4a2ec4ee7231ce93db99f8f0a41dce2e0e
altroz <nhb913@outlook.com>
2015-06-18 23:46:56 -0700

    Quick reference to Reational and Equality Operators.

---
+++ src/chapter04/Operators/OpertorsTest.java
@@ -0,0 +1,21 @@
+package chapter04.Operators;
+
+
+public class OpertorsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a = true;
+		boolean b = false;
+		int aa = 2;
+		int bb = 3;
+		
+		/* >< <= >= != (Relational Operators)
+		 * == and != (Equality Operators)
+		 * 
+		 * When comparing characters, Java uses the Unicode value of the character as
+		 * the numerical value. 
+		 * Four types of things can be tested: numbers, characters, boolean and reference variable*/
+	}
+
+}
