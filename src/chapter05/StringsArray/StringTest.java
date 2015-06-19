a3ff765a72a707ddcb751bae3d498f1d4af52878
altroz <nhb913@outlook.com>
2015-06-19 15:07:15 -0700

    The method called concatation and it invoking + other important methods for String object.

--- src/chapter05/StringsArray/StringTest.java
+++ src/chapter05/StringsArray/StringTest.java
@@ -8,6 +8,20 @@ public class StringTest {
 	String b = new String("abc");
 	String c = "abc";
 	
+	a = a.concat("efg");
+	/*Due to String's immutability, the invocation left us with 
+	 * three object string. 
+	 * 
+	 * 1. object contains ("abc");
+	 * 2. object contains (concat("efg");)
+	 * 2. object contains s.concat("efg");*/
+	
+	/* String object with predefined methods.   */
+	a.toLowerCase();
+	b.toString();
+	c.toUpperCase();
+	a.trim();
+	
 	
 	}
 
