4fe927acc0c7756d4f853bc4b552fb7917a8aabd
altroz <nhb913@outlook.com>
2015-06-20 12:50:35 -0700

    The key points to take away from these examples are that enums can be declared as their own class, or enclosed in another class, and that the syntax for accessing an enum’s members depends on where the enum was declared.

---
+++ src/chapter01/EnumTest/Coffee2.java
@@ -0,0 +1,6 @@
+package chapter01.EnumTest;
+
+public class Coffee2 {
+	enum CoffeeSize{TALL, GRANDE, VENTI}
+	CoffeeSize size;
+}
---
+++ src/chapter01/EnumTest/CoffeeTest2.java
@@ -0,0 +1,8 @@
+package chapter01.EnumTest;
+
+public class CoffeeTest2 {
+	public static void main(String[]args){
+		Coffee2 drink = new Coffee2();
+		drink.size = Coffee2.CoffeeSize.TALL;
+	}
+}
