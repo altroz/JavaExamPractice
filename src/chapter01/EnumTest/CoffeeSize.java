410c1f8a1294489f0d32288b2d5ef9a4ae39cbe4
altroz <nhb913@outlook.com>
2015-06-20 13:01:16 -0700

    Quick Look in invoking constructors from Enum class.

--- src/chapter01/EnumTest/CoffeeSize.java
+++ src/chapter01/EnumTest/CoffeeSize.java
@@ -1,5 +1,16 @@
 package chapter01.EnumTest;
 
 public enum CoffeeSize {
-	TALL, GRANDE, VENTI
+	TALL(12), 
+	GRANDE(16), 
+	VENTI(20);
+	
+	CoffeeSize(int ounces){
+		this.ounces = ounces;
+	}
+	
+	private int ounces;
+	public int getOunces(){
+		return ounces;
+	}
 }
