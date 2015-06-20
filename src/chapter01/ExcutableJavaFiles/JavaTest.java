b350d5933227588f10c698f77bc82981b5a67c6b
altroz <nhb913@outlook.com>
2015-06-20 12:34:11 -0700

    Some executable java files commands regarding - javac : call to Java compiler - java: Call to Java Virtual Machine

---
+++ src/chapter01/ExcutableJavaFiles/JavaTest.java
@@ -0,0 +1,18 @@
+package chapter01.ExcutableJavaFiles;
+
+public class JavaTest {
+	/*
+	 * One can compile and execute java 
+	 * programs using the command-line programs
+	 * of JAVAC and JAVA. 
+	 * 
+	 * javac[Options][Source Files]
+	 * Options: -version and -help
+	 * 
+	 * java[options] class[args]
+	 * 
+	 * Options: -version 
+	 * class[args]: -"call to class of the source file"
+	 * 
+	 * */
+}
