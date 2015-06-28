ba6afd29f6c603021c86101655d0370383fb02e2
altroz <nhb913@outlook.com>
2015-06-27 23:30:19 -0700

    Class Fundalment

---
+++ src/com/OCP1/Circle.java
@@ -0,0 +1,11 @@
+package com.OCP1;
+
+public class Circle {
+	int x, y, radius;  // Instance variable with instance type.
+	
+	void area(){	   // Method called area with a return type of void.
+	}
+	
+	void fillColr() {
+	}
+}
