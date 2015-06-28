929b7659b4b4a29fef022d22c1c11dbcdb0ddbcd
altroz <nhb913@outlook.com>
2015-06-28 10:10:44 -0700

    The usage of static instance member varaible

---
+++ src/com/OCP14/Counter.java
@@ -0,0 +1,18 @@
+package com.OCP14;
+
+public class Counter {
+	private static int counter;
+	public Counter(){
+		counter ++;
+	}
+	public static void printCount(){
+		System.out.println("Number of instance: " + counter);
+	}
+	
+	public static void main(String[]args){
+		Counter anInstance = new Counter();
+		Counter.printCount();
+		Counter anInstance1 = new Counter();
+		Counter.printCount();
+	}
+}
