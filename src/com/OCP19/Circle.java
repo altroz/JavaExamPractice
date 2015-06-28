af748776511f353d8ef0da78cb9b80e1cd50e407
altroz <nhb913@outlook.com>
2015-06-28 13:25:01 -0700

    Circle class implementing Comparable, Iterator and Rollable interface.

--- src/com/OCP19/Circle.java
+++ src/com/OCP19/Circle.java
@@ -1,7 +1,31 @@
 package com.OCP19;
 
-public class Circle implements Rollable{
+public class Circle implements Rollable, Iterator, Comparable{
 	public void roll(float degree){
 		//Implement rolling functionality here
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove() {
+		// TODO Auto-generated method stub
+		
 	}
 }
