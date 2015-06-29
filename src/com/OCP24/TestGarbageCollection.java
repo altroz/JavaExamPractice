6d1408945d034f922c776d8f757760269ba9184b
altroz <nhb913@outlook.com>
2015-06-29 13:42:46 -0700

    Reassigning a Reference Variable to makes objects eligible for Garbage Collection

--- src/com/OCP24/TestGarbageCollection.java
+++ src/com/OCP24/TestGarbageCollection.java
@@ -4,10 +4,13 @@ public class TestGarbageCollection {
 
 	public static void main(String[] args) {
 		StringBuffer sb = new StringBuffer("Hello");
+		StringBuffer sb1 = new StringBuffer("GoodBye");
+		
 		System.out.println(sb);
-		sb = null;
+		System.out.println(sb1);
+		sb = sb1; // StringBuffer "Hello" is eligible for collection. 
 		System.out.println(sb);
-
+		System.out.println(sb1);
 	}
 
 }
