d0c433bccb004f99a8a3f353c81de2bce60d26e9
altroz <nhb913@outlook.com>
2015-06-28 19:07:53 -0700

    Object Oriented Design Pattern

---
+++ src/com/OCP21/Canvas.java
@@ -0,0 +1,8 @@
+package com.OCP21;
+
+public class Canvas {
+	public void update(Circle circle){
+		System.out.println("ShapeArchiver :: Update");
+		//Update Implementation
+	}
+}
---
+++ src/com/OCP21/ShapeArchiver.java
@@ -0,0 +1,8 @@
+package com.OCP21;
+
+public class ShapeArchiver {
+	public void update(Circle circle){
+		System.out.println("ShapeArchiver :: Update");
+		// Update Implementation
+	}
+}
