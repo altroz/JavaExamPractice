686d9ee6e2c13c7a2bd67ed13d432522c7badf11
altroz <nhb913@outlook.com>
2015-06-29 13:26:43 -0700

    Test File Uploaded

---
+++ src/com/OCP22/Test.java
@@ -0,0 +1,14 @@
+package com.OCP22;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Circle circle = new Circle(10,10,20);
+		System.out.println(circle);
+		circle.addObserver(new Canvas());
+		circle.addObserver(new ShapeArchiver());
+		circle.setRadius(50);
+		System.out.println(circle);
+	}
+
+}
