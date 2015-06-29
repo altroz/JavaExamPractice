33409121ae8ad0ff03c1d9f7e6d86947c97fbff7
altroz <nhb913@outlook.com>
2015-06-29 13:13:38 -0700

    Class Point created

---
+++ src/com/OCP22/Point.java
@@ -0,0 +1,15 @@
+package com.OCP22;
+
+public class Point {
+	private int xPos;
+	private int yPos;
+	
+	public Point(int x, int y){
+		xPos = x;
+		yPos = y;
+	}
+	
+	public String toString(){
+		return "( "+ xPos +", "+ yPos+ ")";
+	}
+}
