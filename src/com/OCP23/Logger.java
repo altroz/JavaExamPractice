a0a1f7f7418cc50100b091f95893db54bf4cc317
altroz <nhb913@outlook.com>
2015-06-29 13:33:32 -0700

    Singleton Design Pattern example with comments

--- src/com/OCP23/Logger.java
+++ src/com/OCP23/Logger.java
@@ -1,5 +1,11 @@
 package com.OCP23;
 
+/*  1. Logger class must be instantiated only once in application. It is to ensure
+ * 		that the whole of the application make use of that some logger instance.	
+ * 	2. Declare the constructor private to prevent clients from instantiating an object of this class 
+ * 		directly. 
+ *  3. By default, this field is initialized to NULL
+ *  4. The static method to be used by clients to get the instance of the logger class. */
 public class Logger {
 	private Logger(){	}
 	public static Logger myInstance;
