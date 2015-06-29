2b1d0685534a794a4c0af5aa55731d8cc66f565f
altroz <nhb913@outlook.com>
2015-06-29 13:45:54 -0700

    Using Refence Isolation to explicitly make objects eligible for garbage collection

---
+++ src/com/OCP24/IslandTest.java
@@ -0,0 +1,17 @@
+package com.OCP24;
+
+public class IslandTest {
+	IslandTest i = null;
+	public static void main(String[]args){
+		IslandTest i2 = new IslandTest();
+		IslandTest i3 = new IslandTest();
+		IslandTest i4 = new IslandTest();
+		
+		i2.i = i3;
+		i3.i = i4;
+		i4.i = i2;
+		i2 = null;
+		i3 = null;
+		i4 = null;
+	}
+}
