af4c79cc6b239eb205963069a6038b1789447d6c
altroz <nhb913@outlook.com>
2015-06-29 13:11:21 -0700

    Circle class extends to Observable.

---
+++ src/com/OCP22/Circle.java
@@ -0,0 +1,24 @@
+package com.OCP22;
+
+import java.util.Observable;
+
+public class Circle extends Observable {
+	private Point center;
+
+	public void setCenter(Point center) {
+		this.center = center;
+		setChange();
+		notifyObservers();
+	}
+
+	private int radius;
+
+	public Circle(int x, int y, int r) {
+		center = new Point(x, y);
+		radius = r;
+	}
+
+	public String toString() {
+		return "center = " + center + " and radius = " + radius;
+	}
+}
