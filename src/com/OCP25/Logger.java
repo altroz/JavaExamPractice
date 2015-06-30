143a81c81436637d5f6c1f5eebf025b917da7be0
altroz <nhb913@outlook.com>
2015-06-29 20:06:29 -0700

    Singleton Design Pattern Example

---
+++ src/com/OCP25/Logger.java
@@ -0,0 +1,15 @@
+package com.OCP25;
+
+public class Logger {
+	private Logger(){	}
+	private static Logger myInstance;
+	public static Logger getInstance(){
+		if(myInstance == null){
+			myInstance = new Logger();
+		}
+		return myInstance;
+	}
+	public void log(String[]s){
+		System.err.println(s);
+	}
+}
