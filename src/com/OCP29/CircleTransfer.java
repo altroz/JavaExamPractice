ceff16e3f575297927762be2d05a279702620449
altroz <nhb913@outlook.com>
2015-06-30 12:17:11 -0700

    The CircleTransfer class imcluded .

---
+++ src/com/OCP29/CircleTransfer.java
@@ -0,0 +1,27 @@
+package com.OCP29;
+import java.io.Serializable;
+public class CircleTransfer implements Serializable{
+	private int xPos;
+	private int yPos;
+	private int radius;
+	
+	public void setxPos(int xPos){
+		this.xPos = xPos;
+	}
+	public int getxPos(){
+		return xPos;
+	}
+	
+	public void setyPos(int yPos){
+		this.yPos = yPos;
+	}
+	public int getyPos(){
+		return yPos;
+	}
+	public void setRadius(int radius){
+		this.radius = radius;
+	}
+	public int getRadius(){
+		return radius;
+	}
+}
