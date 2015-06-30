d4b6997b742cb544d457f935d2c5f5b3291d2c99
altroz <nhb913@outlook.com>
2015-06-30 12:10:35 -0700

    RDBMSDAO class implements CircleDAO

---
+++ src/com/OCP29/RDBMSDAO.java
@@ -0,0 +1,13 @@
+package com.OCP29;
+
+public class RDBMSDAO implements CircleDAO {
+	public void insertCircle(CircleTransfer circle){
+		System.out.println("insert Circle Implementation");
+	}
+	public CircleTransfer findCircle(int id){
+		return null;
+	}
+	public void deleteCircle(int id){
+		//delete Circle Implementation
+	}
+}
