5b78699f3f473083f91c4f130afac1a6e292c9ff
altroz <nhb913@outlook.com>
2015-07-02 11:37:43 -0700

    UFOEnemyShip extends to EnemyShip reformatted with same content.

--- src/com/OCP31/UFOEnemyShip.java
+++ src/com/OCP31/UFOEnemyShip.java
@@ -1,7 +1,7 @@
 package com.OCP31;
 
-public class UFOEnemyShip extends EnemyShip{
-	public UFOEnemyShip(){
+public class UFOEnemyShip extends EnemyShip {
+	public UFOEnemyShip() {
 		setName("UFO Enemy Ship");
 		setDamage(20.0);
 	}
