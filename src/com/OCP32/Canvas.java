40accc51ad82c7c6fd50117758ac80371b16e8be
altroz <nhb913@outlook.com>
2015-07-02 15:49:35 -0700

    The canvas where the iteration of the two different types of shape are instantiated through the usage of ShapeFactory class.

---
+++ src/com/OCP32/Canvas.java
@@ -0,0 +1,21 @@
+package com.OCP32;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Canvas {
+	private ArrayList<Shape>shapeList = new ArrayList<Shape>();
+	public void addNewShape(String shapeType){
+		Shape shape = ShapeFactory.getShape(shapeType);
+		shapeList.add(shape);
+	}
+	
+	public void redraw(){
+		Iterator<Shape>itr = shapeList.iterator();
+		while(itr.hasNext()){
+			Shape shape = itr.next();
+			shape.draw();
+		}
+		
+	}
+}
