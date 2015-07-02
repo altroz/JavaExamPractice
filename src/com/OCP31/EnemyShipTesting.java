4248d383a57e1624ac0062c0403fe2074ae70c70
altroz <nhb913@outlook.com>
2015-07-02 11:22:34 -0700

    Recommitted with formatted version but same content.

--- src/com/OCP31/EnemyShipTesting.java
+++ src/com/OCP31/EnemyShipTesting.java
@@ -1,23 +1,30 @@
 package com.OCP31;
+
 import java.util.Scanner;
 
 public class EnemyShipTesting {
-	public static void main(String[]args){
+	public static void main(String[] args) {
 		// Created a Factory Object
 		EnemyShipFactory shipFactory = new EnemyShipFactory();
-		
-		//Enemy ship object
+
+		// Enemy ship object
 		EnemyShip theEnemy = null;
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("What type of ship? (U / R / B)");
-		if(userInput.hasNextLine()){
+		if (userInput.hasNextLine()) {
 			String typeOfShip = userInput.nextLine();
 			theEnemy = shipFactory.makeEnemyShip(typeOfShip);
-			if(theEnemy != null){
-				doStuffEnemy(theEnemy);}
-			else
+			if (theEnemy != null) {
+				doStuffEnemy(theEnemy);
+			} else
 				System.out.println("Please enter U, R, or B next time");
-			}
 		}
 	}
+
+	public static void doStuffEnemy(EnemyShip anEnemyShip) {
+		anEnemyShip.displayEnemyShip();
+		anEnemyShip.followHeroShip();
+		anEnemyShip.enemyShipShoots();
+	}
+
 }
