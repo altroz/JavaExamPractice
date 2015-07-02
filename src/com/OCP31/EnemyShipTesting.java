5ad3cd187a4b989d3ac0bc438101eecfd45a4e9c
altroz <nhb913@outlook.com>
2015-07-02 11:12:59 -0700

    This is the test class to illustrate the usage of Factory Design Pattern. The class initiates a Factory Object called shipFacotry. The second takes the user input for the arguement typeOfShip for makeEnemyShip.

---
+++ src/com/OCP31/EnemyShipTesting.java
@@ -0,0 +1,23 @@
+package com.OCP31;
+import java.util.Scanner;
+
+public class EnemyShipTesting {
+	public static void main(String[]args){
+		// Created a Factory Object
+		EnemyShipFactory shipFactory = new EnemyShipFactory();
+		
+		//Enemy ship object
+		EnemyShip theEnemy = null;
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("What type of ship? (U / R / B)");
+		if(userInput.hasNextLine()){
+			String typeOfShip = userInput.nextLine();
+			theEnemy = shipFactory.makeEnemyShip(typeOfShip);
+			if(theEnemy != null){
+				doStuffEnemy(theEnemy);}
+			else
+				System.out.println("Please enter U, R, or B next time");
+			}
+		}
+	}
+}
