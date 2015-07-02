56589adaf2bba6f1f4ab70b4cdaca2fa5a3086be
altroz <nhb913@outlook.com>
2015-07-02 15:53:54 -0700

    The test class to test the all the other classes as part of the factory design pattern.

---
+++ src/com/OCP32/Test.java
@@ -0,0 +1,10 @@
+package com.OCP32;
+
+public class Test {
+	public static void main(String[]args){
+		Canvas canvas = new Canvas();
+		canvas.addNewShape("Circle");
+		canvas.addNewShape("Rectangle");
+		canvas.redraw();
+	}
+}
