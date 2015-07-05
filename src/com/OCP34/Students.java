21183549f0e200c3e9b9c0816c5611d5350b2387
altroz <nhb913@outlook.com>
2015-07-04 23:16:35 -0700

    The following program illustrates the usage of Comparable interface.

---
+++ src/com/OCP34/Students.java
@@ -0,0 +1,22 @@
+package com.OCP34;
+
+import java.util.*;
+public class Students implements Comparable<Students>{
+	String id;
+	String name;
+	Double cgpa;
+	
+	public Students(String studentID, String studentName, double studentCGPA){
+		id = studentID;
+		name = studentName;
+		cgpa = studentCGPA;
+	}
+	
+	public String toString(){
+		return "\n"+id+"\t"+name+"\t"+cgpa;
+	}
+	public int compareTo(Students that){
+		return this.id.compareTo(that.id);
+	}
+	
+}
