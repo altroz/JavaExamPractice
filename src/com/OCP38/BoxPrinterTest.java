fcec55341fff6d5e1b26e433326b59fd96fb45d0
altroz <nhb913@outlook.com>
2015-07-06 12:01:13 -0700

    Using generic type to test the BoxPrinter

--- src/com/OCP38/BoxPrinterTest.java
+++ src/com/OCP38/BoxPrinterTest.java
@@ -5,7 +5,10 @@ public class BoxPrinterTest {
 		BoxPrinter<Integer>value1 = new BoxPrinter<Integer>(new Integer(10));
 		System.out.println(value1);
 		Integer intValue1 = value1.getValue();
+		System.out.println(intValue1);
 		BoxPrinter<String>value2 = new BoxPrinter<String>("Hello World");
 		String intValue2 = value2.getValue();
+		System.out.println(intValue2);
+		System.out.println(value2);
 		}
 }
