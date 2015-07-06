52587993470fb5d88c756b7ce1c19616e7a81e43
altroz <nhb913@outlook.com>
2015-07-06 12:08:58 -0700

    Testing the Pair class with two types of client/user entered type variable.

---
+++ src/com/OCP39/PairTest.java
@@ -0,0 +1,8 @@
+package com.OCP39;
+
+public class PairTest {
+	public static void main(String[]args){
+		Pair<Integer,String>worldCup = new Pair<Integer,String>(2010, "South Africa");
+		System.out.println("World Cup "+ worldCup.getFirst() + " in "+ worldCup.getSecond());
+	}
+}
