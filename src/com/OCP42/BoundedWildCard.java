59b573ada8f8cf4d541ae83a6b8a98c4a914cb0a
altroz <nhb913@outlook.com>
2015-07-06 15:26:03 -0700

    testing the boundedWildCard

--- src/com/OCP42/BoundedWildCard.java
+++ src/com/OCP42/BoundedWildCard.java
@@ -10,4 +10,17 @@ public class BoundedWildCard {
 		}
 		return result;
 	}
+	public static void main(String[]args){
+		List<Integer> intList = new ArrayList<Integer>();
+		List<Double> doubleList = new ArrayList<Double>();
+		
+		for(int i = 0; i < 5; i++){
+			intList.add(i);
+			doubleList.add((double)(i*i));
+		}
+		System.out.println("The intList is: "+ intList);
+		System.out.println("The sum of elements in intList is: "+ sum(intList));
+		System.out.println("The doubleList is: "+ doubleList);
+		System.out.println("The sum of elements in doubleList is: "+ sum(doubleList));
+	}
 }
