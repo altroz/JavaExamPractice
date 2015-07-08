72a342384353a361a09a3940940b9a74ffa66500
altroz <nhb913@outlook.com>
2015-07-08 09:33:58 -0700

    Search String using indexOf() method

---
+++ src/com/OCP47/SearchString1.java
@@ -0,0 +1,9 @@
+package com.OCP47;
+
+public class SearchString1 {
+	public static void main(String[] args) {
+		String str = "OCPJP";
+		System.out.println("Character \'J\' occurs at index:  "
+				+ str.indexOf("J"));
+	}
+}
