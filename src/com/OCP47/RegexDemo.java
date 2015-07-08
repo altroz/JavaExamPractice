1a5bfe3c51dca2436fa4cc275fb6eb67d44e0946
altroz <nhb913@outlook.com>
2015-07-08 10:14:36 -0700

    Using the Matcher and Pattern regex to find the PO box of the given clients.

---
+++ src/com/OCP47/RegexDemo.java
@@ -0,0 +1,19 @@
+package com.OCP47;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexDemo {
+	public static void main(String[]args){
+		String str = "Danny Doo, Flat no 502, Big Apartment, Wide Road, Near Huge Milestone, Hugo-city 56100,"
+				+ "PH 2134015397, email: dannyword@world.com, Maggi Myer, Post Bang no 54, Big Bank Post Office,"
+				+ "Big Bank City 86000, PH 2134018499, email: naggi07@myuniverse.com.";
+		
+		Pattern pattern = Pattern.compile("\\b\\d{5}\\b");
+		Matcher matcher = pattern.matcher(str);
+		
+		while(matcher.find()){
+			System.out.println(matcher.group());
+		}
+	}
+}
