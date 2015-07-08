7dfd23db8c4138e4f7f700b2c681a6c81d47cf60
altroz <nhb913@outlook.com>
2015-07-08 09:36:48 -0700

    using the indexOf() method for finding substrings

---
+++ src/com/OCP47/SearchString2.java
@@ -0,0 +1,9 @@
+package com.OCP47;
+
+public class SearchString2 {
+	public static void main(String[] args) {
+		String str = "I am preparing for OCJP";
+		System.out.println("Substring \'for \' occurs at index: "
+				+ str.indexOf("for"));
+	}
+}
