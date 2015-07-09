e8107c5eb213951df8ea10ef3393c636de26929c
altroz <nhb913@outlook.com>
2015-07-09 10:49:57 -0700

    reformmated with same content.

--- src/basicDesignPattern/WorkingWithAnimal.java
+++ src/basicDesignPattern/WorkingWithAnimal.java
@@ -8,19 +8,19 @@ public class WorkingWithAnimal {
 		fido.digHole();
 		fido.setWeight(50);
 		System.out.println("The weight of the Dog is: " + fido.getWeight());
-		
+
 		Animal doggy = new Dog();
 		Animal kitty = new Cat();
-		
-		System.out.println("Dog says: "+ doggy.getSound());
-		System.out.println("Cat says: "+ kitty.getSound());
-		
-		((Dog)doggy).digHole();
-		
+
+		System.out.println("Dog says: " + doggy.getSound());
+		System.out.println("Cat says: " + kitty.getSound());
+
+		((Dog) doggy).digHole();
+
 		Animal[] animal = new Animal[2];
-		animal[0] =  doggy;
+		animal[0] = doggy;
 		animal[1] = kitty;
-		for(int i = 0; i<animal.length;i++){
+		for (int i = 0; i < animal.length; i++) {
 			System.out.println(animal[i].getSound());
 		}
 	}
