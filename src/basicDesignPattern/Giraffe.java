cc40b4b1b2ef44c825c2b5c3b627181c301e53e8
altroz <nhb913@outlook.com>
2015-07-09 11:04:43 -0700

    The giraffe class has implmented the abstract methods from the abstract class of Creature.

---
+++ src/basicDesignPattern/Giraffe.java
@@ -0,0 +1,30 @@
+package basicDesignPattern;
+
+public class Giraffe extends Creature{
+	private String name;
+	private double weight;
+	@Override
+	public void setName(String newName) {
+		// TODO Auto-generated method stub
+		name = newName;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+
+	@Override
+	public void setWeight(double newWeight) {
+		// TODO Auto-generated method stub
+		weight = newWeight;
+	}
+
+	@Override
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return weight;
+	}
+	
+}
