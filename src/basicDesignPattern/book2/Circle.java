010da1c76c22bbb20cb559f2f060a0c51cb048b1
altroz <nhb913@outlook.com>
2015-07-10 13:28:18 -0700

    reformatted with same content.

--- src/basicDesignPattern/book2/Circle.java
+++ src/basicDesignPattern/book2/Circle.java
@@ -1,24 +1,25 @@
 package basicDesignPattern.book2;
 
-public class Circle extends Shape implements Rollable{
+public class Circle extends Shape implements Rollable {
 	private int xPos, yPos, radius;
-	
-	public Circle(int x, int y, int r){
+
+	public Circle(int x, int y, int r) {
 		xPos = x;
 		yPos = y;
 		radius = r;
 	}
-	public double area(){
-		return Math.PI * radius * radius ;
+
+	public double area() {
+		return Math.PI * radius * radius;
 	}
-	
-	public void roll(float degree){
-		//implement rolling functionality
+
+	public void roll(float degree) {
+		// implement rolling functionality
 	}
-	public static void main(String[]args){
-		Circle circle = new Circle(10,10,20);
+
+	public static void main(String[] args) {
+		Circle circle = new Circle(10, 10, 20);
 		circle.roll(45);
 	}
 
-	
 }
