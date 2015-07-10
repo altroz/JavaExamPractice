80ac7824319e39090f18c91e70ee41a7dba8883a
altroz <nhb913@outlook.com>
2015-07-10 14:19:11 -0700

    MergeSort implements the Sorting class through implementing the method. Plus the method returns the list.

---
+++ src/composition/Inheritance/MergeSort.java
@@ -0,0 +1,10 @@
+package composition.Inheritance;
+
+import java.awt.*;
+
+public class MergeSort implements Sorting {
+	public List sort(List list) {
+		// set implementation
+		return list;
+	}
+}
