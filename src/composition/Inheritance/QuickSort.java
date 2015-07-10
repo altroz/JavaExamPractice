33848980ea50cb9116b527aca70e791f8ea44536
altroz <nhb913@outlook.com>
2015-07-10 14:28:07 -0700

    QuickSort implements Sorting interface method called Sort with a return type of List.

---
+++ src/composition/Inheritance/QuickSort.java
@@ -0,0 +1,13 @@
+package composition.Inheritance;
+
+import java.awt.List;
+
+public class QuickSort implements Sorting {
+
+	@Override
+	public List sort(List list) {
+		// TODO Auto-generated method stub
+		return list;
+	}
+
+}
