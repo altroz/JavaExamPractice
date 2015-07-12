856ed8b11d3d2ce54c0626212ce9b60359f0a3e4
altroz <nhb913@outlook.com>
2015-07-11 23:46:10 -0700

    Recommitted and cleared

--- src/chapter02/Polymorphism/Car.java
+++
@@ -1,7 +0,0 @@
-package chapter02.Polymorphism;
-
-public class Car {
-	public void goForADrive(){
-		System.out.println("Driving A Car");
-	}
-}
--- src/chapter02/Polymorphism/Convertible.java
+++
@@ -1,5 +0,0 @@
-package chapter02.Polymorphism;
-
-public interface Convertible {
-	public void openHood();
-}
