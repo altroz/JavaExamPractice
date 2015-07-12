70d7fdeb15966c38224b499cb33f1ae070ad0fa8
altroz <nhb913@outlook.com>
2015-07-11 23:45:45 -0700

    Recommitted and cleared

--- src/chapter02/Inheritance2/Car.java
+++
@@ -1,7 +0,0 @@
-package chapter02.Inheritance2;
-
-public class Car {
-	 void drive(){
-		System.out.println("Generic Noise");
-	}
-}
--- src/chapter02/Inheritance2/Ferrari.java
+++
@@ -1,6 +0,0 @@
-package chapter02.Inheritance2;
-
-public class Ferrari extends Car{
-	void drive(){System.out.println("Drive");}
-	void brake(){System.out.println("Brake");}
-}
