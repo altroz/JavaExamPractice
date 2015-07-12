538520aafa243e88fee8ebd328c7db163d570889
altroz <nhb913@outlook.com>
2015-07-11 23:29:06 -0700

    TestToStringMethod

---
+++ src/collections/book1/TestToStringMethod.java
@@ -0,0 +1,8 @@
+package collections.book1;
+
+public class TestToStringMethod {
+	public static void main(String[] args) {
+		TestToStringMethod h = new TestToStringMethod();
+		System.out.println(h);
+	}
+}
