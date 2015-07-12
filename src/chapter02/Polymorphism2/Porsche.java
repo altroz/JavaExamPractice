8b50fbe53f62f1711266785e86fe932cd6ebfbb7
altroz <nhb913@outlook.com>
2015-07-11 23:47:12 -0700

    Recommitted and cleared

--- src/chapter02/Polymorphism2/Porsche.java
+++
@@ -1,7 +0,0 @@
-package chapter02.Polymorphism2;
-
-public class Porsche extends Car{
-	public void changeGear(){
-		super.changeGear();
-	}
-}
--- src/chapter02/Polymorphism2/TestCar.java
+++
@@ -1,8 +0,0 @@
-package chapter02.Polymorphism2;
-
-public class TestCar {
-	public static void main(String [] args){
-		Car a = new Porsche();
-		a.changeGear();
-	}
-}
