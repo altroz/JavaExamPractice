60ea1f51d7140050a084ccc9f2aca4a0b14b88d7
altroz <nhb913@outlook.com>
2015-07-11 21:10:42 -0700

    Circle Class that will play an important role for the Data Object Access Design Pattern

---
+++ src/dataObjectAccessDesignPattern/Circle.java
@@ -0,0 +1,26 @@
+package dataObjectAccessDesignPattern;
+
+import com.OCP29.CircleTransfer;
+
+public class Circle {
+	private int xPos, yPos, radius;
+
+	public Circle(int x, int y, int r) {
+		xPos = x;
+		yPos = y;
+		radius = r;
+	}
+
+	public String toString() {
+		return "center = ( " + xPos + ", " + yPos + ")";
+	}
+
+	public CircleTransfer getCircleTransfer() {
+		CircleTransfer circleTransfer = new CircleTransfer();
+		circleTransfer.setRadius(radius);
+		circleTransfer.setxPos(xPos);
+		circleTransfer.setyPos(yPos);
+
+		return circleTransfer;
+	}
+}
