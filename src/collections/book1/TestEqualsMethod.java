ed154c52c632f9d9d62fcdf866fcca034797a662
altroz <nhb913@outlook.com>
2015-07-11 23:36:35 -0700

    Overriding equals method.

---
+++ src/collections/book1/TestEqualsMethod.java
@@ -0,0 +1,13 @@
+package collections.book1;
+
+public class TestEqualsMethod {
+	public static void main(String[]args){
+		TestClass one = new TestClass(8);
+		TestClass two = new TestClass(8);
+		
+	if(one.equals(two)){
+		System.out.println("one and two are equal");	
+	}
+	else System.out.println("one and two are not equal");
+	}
+}
