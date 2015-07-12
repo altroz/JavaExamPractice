27eb0fd8f2639a2d7e76d3e740361b2c0eadad02
altroz <nhb913@outlook.com>
2015-07-11 23:40:31 -0700

    TestClass setted up with equals methods overridden.

---
+++ src/collections/book1/TestClass.java
@@ -0,0 +1,22 @@
+package collections.book1;
+
+public class TestClass {
+	private int TestClassValue;
+
+	TestClass(int val) {
+		TestClassValue = val;
+	}
+
+	public int getTestClassValue() {
+		return TestClassValue;
+	}
+
+	public boolean equals(Object o) {
+		if ((o instanceof TestClass)
+				&& (((TestClass) o).getTestClassValue() == this.TestClassValue)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
