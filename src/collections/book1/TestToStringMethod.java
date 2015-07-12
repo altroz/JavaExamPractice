761204e5810a360fb2bd2631df61a6de344e434a
altroz <nhb913@outlook.com>
2015-07-11 23:31:07 -0700

    Overriding the toString() method.

--- src/collections/book1/TestToStringMethod.java
+++ src/collections/book1/TestToStringMethod.java
@@ -2,7 +2,7 @@ package collections.book1;
 
 public class TestToStringMethod {
 	public static void main(String[] args) {
-		TestToStringMethod h = new TestToStringMethod();
-		System.out.println(h);
+		ChildClass f = new ChildClass("GoChildClassGo", 19);
+		System.out.println(f);
 	}
 }
