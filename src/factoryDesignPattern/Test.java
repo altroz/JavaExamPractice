4b0123286e93d3846eeaabe6cd0fa4de6a52c685
altroz <nhb913@outlook.com>
2015-07-11 22:49:30 -0700

    Test class created and it is successfull;

---
+++ src/factoryDesignPattern/Test.java
@@ -0,0 +1,10 @@
+package factoryDesignPattern;
+
+public class Test {
+	public static void main(String[]args){
+		Canvas canvas = new Canvas();
+		canvas.addNewShape("Circle");
+		canvas.addNewShape("Rectangle");
+		canvas.redraw();
+	}
+}
