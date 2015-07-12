23754c978acf5c7ab7c1c2a30fbd3d73ee7a8464
altroz <nhb913@outlook.com>
2015-07-11 23:45:22 -0700

    Recommitted and cleared

--- src/chapter02/Inheritance1/Ferrari.java
+++
@@ -1,7 +0,0 @@
-package chapter02.Inheritance1;
-
-public class Ferrari extends Car{
-	public void takeFerrariForADrive(){
-		System.out.println("Driving A Ferrari");
-	}
-}
--- src/chapter02/Inheritance1/Porsche.java
+++
@@ -1,7 +0,0 @@
-package chapter02.Inheritance1;
-
-public class Porsche extends Car {
-	public void takePorscheForADrive(){
-		System.out.println("Driving A Porsche");
-	}
-}
