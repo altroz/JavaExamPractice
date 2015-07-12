88d5356476936e48be68aa41caf97790c820317f
altroz <nhb913@outlook.com>
2015-07-11 23:47:30 -0700

    Recommitted and cleared

--- src/chapter02/Polymorphism3/Car.java
+++
@@ -1,7 +0,0 @@
-package chapter02.Polymorphism3;
-
-public class Car {
-	public void drive() throws Exception{
-		// throw Exception codes
-	}
-}
--- src/chapter02/Polymorphism4/Car.java
+++
@@ -1,5 +0,0 @@
-package chapter02.Polymorphism4;
-
-public class Car {
-	public void drive(){}
-}
