1690db69aa947a1223acc93fb4e9b3e0827a1ea0
altroz <nhb913@outlook.com>
2015-07-11 21:44:06 -0700

    RDMSDAO class implements CircleDAO.

---
+++ src/dataObjectAccessDesignPattern/RDBMSDAO.java
@@ -0,0 +1,15 @@
+package dataObjectAccessDesignPattern;
+
+public class RDBMSDAO implements CircleDAO {
+	public void insertCircle(CircleTransfer circle{
+		System.out.println("insertCircle implementation");
+		// further implementation algorithms
+	}
+	public Circle findCircle(int id){
+		// findCircle implementation
+		return null;
+	}
+	public void deleteCircle(int id){
+		// deleteCircle implementation algorithms. 
+	}
+}
