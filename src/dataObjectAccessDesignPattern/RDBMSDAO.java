876ae36dc6454aa0202bd2f766b3491a31fcb75c
altroz <nhb913@outlook.com>
2015-07-11 21:53:43 -0700

    Recommitted with correct codes.

--- src/dataObjectAccessDesignPattern/RDBMSDAO.java
+++ src/dataObjectAccessDesignPattern/RDBMSDAO.java
@@ -1,15 +1,17 @@
 package dataObjectAccessDesignPattern;
 
 public class RDBMSDAO implements CircleDAO {
-	public void insertCircle(CircleTransfer circle{
+	public void insertCircle(CircleTransfer circle){
 		System.out.println("insertCircle implementation");
 		// further implementation algorithms
 	}
-	public Circle findCircle(int id){
+	public CircleTransfer findCircle(int id){
 		// findCircle implementation
 		return null;
 	}
 	public void deleteCircle(int id){
 		// deleteCircle implementation algorithms. 
 	}
+
+	
 }
