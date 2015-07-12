a57678690ce7ad0fa858b289d6394bd41bc1766b
altroz <nhb913@outlook.com>
2015-07-11 21:41:08 -0700

    interface CircleDAO created

---
+++ src/dataObjectAccessDesignPattern/CircleDAO.java
@@ -0,0 +1,7 @@
+package dataObjectAccessDesignPattern;
+
+public interface CircleDAO {
+	public void insertCircle(CircleTransfer circle);
+	public CircleTransfer findCircle(int id);
+	public void deleteCircle(int d);
+}
