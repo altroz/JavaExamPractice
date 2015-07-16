ee0d8c4d47d3b00093a7a47b9c1c78bb551122b4
altroz <nhb913@outlook.com>
2015-07-16 00:02:52 -0700

    executing the main thread after the execution of timer. With the help of try and catch block with the help of timer.join();

--- src/ThreadExamples/TimeBomb.java
+++ src/ThreadExamples/TimeBomb.java
@@ -2,25 +2,30 @@ package ThreadExamples;
 
 import java.lang.*;
 
-class TimeBomb extends Thread{
-	String[] timeStr = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+class TimeBomb extends Thread {
+	String[] timeStr = { "zero", "one", "two", "three", "four", "five", "six",
+			"seven", "eight", "nine" };
 
-	public void run(){
-		for(int i = 9; i>=0; i--){
-			try{
+	public void run() {
+		for (int i = 9; i >= 0; i--) {
+			try {
 				System.out.println(timeStr[i]);
 				Thread.sleep(1000);
-			}
-			catch(InterruptedException ie){
+			} catch (InterruptedException ie) {
 				ie.printStackTrace();
 			}
 		}
 	}
 
-	public static void main(String[]args){
+	public static void main(String[] args) {
 		TimeBomb timer = new TimeBomb();
 		System.out.println("Starting 10 seconds count down");
 		timer.start();
+		try {
+			timer.join();
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
+		}
 		System.out.println("BOMMMMM!");
 	}
 }
