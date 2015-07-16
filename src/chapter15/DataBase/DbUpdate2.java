f9dd1d26deea6d193a66a630ef357fc80649df14
altroz <nhb913@outlook.com>
2015-07-15 18:17:43 -0700

    Using Java Data Base Connectivitiy API to manipulate and insert data in the mySQL database.

---
+++ src/chapter15/DataBase/DbUpdate2.java
@@ -0,0 +1,44 @@
+package chapter15.DataBase;
+
+import java.io.IOException;
+import java.sql.*;
+
+import chapter15.DbConnect1.DbConnector;
+
+public class DbUpdate2 {
+	public static void main(String[] args) throws SQLException {
+		try (Connection connection = DbConnector.connectToDb();
+				Statement statement = connection.createStatement(
+						ResultSet.TYPE_SCROLL_SENSITIVE,
+						ResultSet.CONCUR_UPDATABLE);
+				ResultSet resultSet = statement
+						.executeQuery("SELECT * FROM contact WHERE firstName =\"Michael\"")) {
+			System.out.println("Before the update");
+			System.out.println("id\tfName\tlName\tEmail\t\tphoneNo.");
+
+			while (resultSet.next()) {
+				System.out.println(resultSet.getInt("id") + "\t"
+						+ resultSet.getString("firstName") + "\t"
+						+ resultSet.getString("lastName") + "\t"
+						+ resultSet.getString("Email") + "\t"
+						+ resultSet.getString("phoneNo"));
+			}
+			resultSet.absolute(1);
+			resultSet.updateString("phoneNo", "=919876543210");
+			resultSet.updateRow();
+			System.out.println("After the Update");
+			System.out.println("id\tfName\tlName\tEmail\t\tphoneNo.");
+			resultSet.beforeFirst();
+			while (resultSet.next()) {
+				System.out.println(resultSet.getInt("id") + "\t"
+						+ resultSet.getString("firstName") + "\t"
+						+ resultSet.getString("lastName") + "\t"
+						+ resultSet.getString("Email") + "\t"
+						+ resultSet.getString("phoneNo"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+}
