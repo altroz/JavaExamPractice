786da67d0b178f74d1670c1fd3f92faa0bfe0719
altroz <nhb913@outlook.com>
2015-07-15 23:54:45 -0700

    Quick Fix

---
+++ src/ThreadExamples/MyThread3.java
@@ -0,0 +1,17 @@
+package ThreadExamples;
+
+import java.lang.*;
+
+public class MyThread3 implements Runnable {
+	public void run() {
+		System.out.println("In run method; thread name is: "
+				+ Thread.currentThread().getName());
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread myThread = new Thread(new MyThread3());
+		myThread.run(); // note run() instead of start() here
+		System.out.println("In main method; thread name is: "
+				+ Thread.currentThread().getName());
+	}
+}
