5fd591a6f2533767198a177b9014c38e6f232185
altroz <nhb913@outlook.com>
2015-07-16 15:13:18 -0700

    DataRace class executes the Counter and UserCounter

---
+++ src/chapter13/Synchronization1/DataRace.java
@@ -0,0 +1,14 @@
+package chapter13.Synchronization1;
+
+public class DataRace {
+	public static void main(String[] args) {
+		UseCounter c = new UseCounter();
+		Thread t1 = new Thread(c);
+		Thread t2 = new Thread(c);
+		Thread t3 = new Thread(c);
+
+		t1.start();
+		t1.start();
+		t1.start();
+	}
+}
