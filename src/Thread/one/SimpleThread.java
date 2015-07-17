5a4c1a6253794408b26b53fbcb7fd5a5cb6ad699
altroz <nhb913@outlook.com>
2015-07-17 12:29:36 -0700

    Recommitted with slight changes for experimentation purposes.

--- src/Thread/one/SimpleThread.java
+++ src/Thread/one/SimpleThread.java
@@ -3,8 +3,9 @@ package Thread.one;
 public class SimpleThread {
 	public static void main(String[] args) {
 		Thread t = new Thread();
-
+		Thread a = new Thread();
 		System.out.println(t);
+		System.out.println(a);
 
 	}
 }
