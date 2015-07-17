f617420d9106305c0ddbe6b08a53e7d6bfc18f8d
altroz <nhb913@outlook.com>
2015-07-17 14:20:14 -0700

    DataRace class created to execute the DataRace program.

---
+++ src/Thread/two/DataRace.java
@@ -0,0 +1,14 @@
+package Thread.two;
+
+public class DataRace {
+	public static void main(String[] args) {
+		UseCounter c = new UseCounter();
+		Thread t1 = new Thread(c);
+		Thread t2 = new Thread(c);
+		Thread t3 = new Thread(c);
+		t1.start();
+		t2.start();
+		t3.start();
+
+	}
+}
