81a129ba602ac762646e212933e4db820e997d14
altroz <nhb913@outlook.com>
2015-07-17 12:31:51 -0700

    Slight changes with the same components

--- src/Thread/one/SimpleThread.java
+++ src/Thread/one/SimpleThread.java
@@ -4,8 +4,12 @@ public class SimpleThread {
 	public static void main(String[] args) {
 		Thread t = new Thread();
 		Thread a = new Thread();
+		Thread b = new Thread();
+		Thread c = new Thread();
 		System.out.println(t);
 		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
 
 	}
 }
