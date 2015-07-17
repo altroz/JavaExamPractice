ceae9f88690884efae070cb26fcab50e88e741a3
altroz <nhb913@outlook.com>
2015-07-16 19:45:37 -0700

    Gamers class created

---
+++ src/chapter13/DiceGame/Gamers.java
@@ -0,0 +1,11 @@
+package chapter13.DiceGame;
+
+public class Gamers {
+	// prevent instantiating this utility class by making
+	// constructor private
+	private Gamers() {
+	}
+
+	public static final String JOE = "Joe";
+	public static final String JANE = "Jane";
+}
