8c46c5a6a5c76bc2dbff6d973d31e913b0ba7c59
altroz <nhb913@outlook.com>
2015-07-18 16:59:19 -0700

    test toString() method again

---
+++ src/generic/one/TestToStringAgain.java
@@ -0,0 +1,8 @@
+package generic.one;
+
+public class TestToStringAgain {
+	public static void main(String[] args) {
+		ChildClass f = new ChildClass("Nazmul Haque Biswas", 25);
+		System.out.println(f);
+	}
+}
