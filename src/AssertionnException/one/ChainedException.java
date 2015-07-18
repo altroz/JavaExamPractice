e1c468dd9815e9940b320bde838ad13d6e297d3e
altroz <nhb913@outlook.com>
2015-07-18 12:18:33 -0700

    Illustrates how a method can throw an exception and it is handled and managed by a catch block

---
+++ src/AssertionnException/one/ChainedException.java
@@ -0,0 +1,27 @@
+package AssertionnException.one;
+
+public class ChainedException {
+	public static void foo() {
+		try {
+			String[] str = { "foo" };
+			System.out.println("About to throw ArrayIndexOutOfBoundException");
+			// following statement has out-of-bound access
+			String functionName = str[10];
+		} catch (ArrayIndexOutOfBoundsException oob) {
+			System.out
+					.println("Wrapping ArrayIndexOutOfBoundException into a RuntimeException");
+			throw new RuntimeException(oob);
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			foo();
+		} catch (Exception re) {
+			System.out.println("The caught exception in main is: "
+					+ re.getClass());
+			System.out.println("The cause of the Exception is: "
+					+ re.getCause());
+		}
+	}
+}
