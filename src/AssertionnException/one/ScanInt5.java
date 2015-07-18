80e2311a7164bf70215191491a8cffa46edcd32a
altroz <nhb913@outlook.com>
2015-07-18 12:06:47 -0700

    Handling multiple exceptions with one catch block.

---
+++ src/AssertionnException/one/ScanInt5.java
@@ -0,0 +1,20 @@
+package AssertionnException.one;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class ScanInt5 {
+	public static void main(String[] args) {
+		String integerStr = " ";
+		System.out.println("The string to scan integer from it is: "
+				+ integerStr);
+		Scanner consoleScanner = new Scanner(integerStr);
+		try {
+			System.out.println("The integer value scanned from string is: "
+					+ consoleScanner.nextInt());
+		} catch (NoSuchElementException | IllegalStateException multi) {
+			System.out
+					.println("Error: An error occured while attempting to scan the integer");
+		}
+	}
+}
