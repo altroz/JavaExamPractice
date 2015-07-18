7ca98d60ed38ebb4fb3140952296f61695edebbe
altroz <nhb913@outlook.com>
2015-07-18 16:56:49 -0700

    ChildClass with constructors and toString() overridden

---
+++ src/generic/one/ChildClass.java
@@ -0,0 +1,16 @@
+package generic.one;
+
+public class ChildClass {
+	int age;
+	String name;
+
+	ChildClass(String name, int age) {
+		this.age = age;
+		this.name = name;
+	}
+
+	public String toString() {
+		return ("I am a ChildClass, but you can call me " + name
+				+ ". My age is " + age);
+	}
+}
