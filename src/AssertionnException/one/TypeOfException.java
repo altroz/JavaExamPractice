12ec586498c1d78d8b1adcd3e0301559ae1f0dc2
altroz <nhb913@outlook.com>
2015-07-17 20:01:58 -0700

    Types of Exception.

---
+++ src/AssertionnException/one/TypeOfException.java
@@ -0,0 +1,15 @@
+package AssertionnException.one;
+
+public class TypeOfException {
+	/*
+	 * 1.ArrayIndexOutOfBoundsExceptions 2.ClassCastException
+	 * 3.IllegalArguementException 4.IllegalStateException
+	 * 5.NullPointerException 6.NumberFormatException 7.AssertionError
+	 * 8.ExceptionInInitializerError 9.StackOverFlowError 10.NoClassDefError
+	 */
+
+	public static void main(String[] args) {
+		String s = " ";
+		System.out.println(s.length());
+	}
+}
