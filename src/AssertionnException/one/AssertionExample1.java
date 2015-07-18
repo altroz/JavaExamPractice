46c0c1005f04d13e6a3184bd7ee88a4c578617fe
altroz <nhb913@outlook.com>
2015-07-18 11:08:26 -0700

    Recommit with same content and format.

--- src/AssertionnException/one/AssertionExample1.java
+++ src/AssertionnException/one/AssertionExample1.java
@@ -7,7 +7,7 @@ public class AssertionExample1 {
 			// if negative value. cnvert into positive value
 			
 		}
-		System.out.println("the value of i is " + i);
+	System.out.println("the value of i is " + i);
 		// at this point the assumption is that i cannot be negative.
 		assert (i >= 0) : "impossible: i is negative. ";
 	}
