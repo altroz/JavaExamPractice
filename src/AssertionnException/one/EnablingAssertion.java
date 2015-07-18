aa523cec2d61b9b8f707d5928e7d7db554feebf2
altroz <nhb913@outlook.com>
2015-07-17 19:54:59 -0700

    Quick view of difference between keywords and identifiers. Now-a-days keywords are want assert are used as.

---
+++ src/AssertionnException/one/EnablingAssertion.java
@@ -0,0 +1,14 @@
+package AssertionnException.one;
+
+public class EnablingAssertion {
+	void testMethod(){
+		int assert = getInitialValue();
+		if(assert == getInitialValue()){
+			
+		}
+	}
+	
+	void int getInitialValue(){
+		return 0;
+	}
+}
