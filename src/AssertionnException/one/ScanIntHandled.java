99e757d310e548d0575cdb1a7d654d526fd5fc74
altroz <nhb913@outlook.com>
2015-07-18 11:39:32 -0700

    Handled Exceptions. The thrown exception by .nextInt() is InputMisMatchException which is handled in the catch block .

---
+++ src/AssertionnException/one/ScanIntHandled.java
@@ -0,0 +1,22 @@
+package AssertionnException.one;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ScanIntHandled {
+	public static void main(String[] args) {
+		System.out.println("Type an integer in the console: ");
+		Scanner consoleScanner = new Scanner(System.in);
+
+		try {
+			System.out.println("Your typed the integer value: "
+					+ consoleScanner.nextInt());
+		} catch (InputMismatchException ime) {
+			// .nextInt() throws InputMisMatchException in case anything other
+			// than an integer.
+			// is typed in the console; so handle it.
+			System.out
+					.println("Error: You typed some text that is not an integer value. ");
+		}
+	}
+}
