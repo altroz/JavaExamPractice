7012ae11aa535a6296e97465ad9c383655f2953e
altroz <nhb913@outlook.com>
2015-07-18 16:57:29 -0700

    hashCode() overridden

---
+++ src/generic/one/HashTest.java
@@ -0,0 +1,23 @@
+package generic.one;
+
+public class HashTest {
+	public int x;
+
+	HashTest(int xVal) {
+		x = xVal;
+	}
+
+	public boolean equals(Object o) {
+		HashTest h = (HashTest) o;
+
+		if (h.x == this.x) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int hashCode() {
+		return (x * 17);
+	}
+}
