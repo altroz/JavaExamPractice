44e3d97a9ca062ad403a71d08e09833a68d0b3d0
altroz <nhb913@outlook.com>
2015-07-18 11:02:23 -0700

    Assertion example for testing your assumptions.

---
+++ src/AssertionnException/one/AssertionExample1.java
@@ -0,0 +1,14 @@
+package AssertionnException.one;
+
+public class AssertionExample1 {
+	public static void main(String[] args) {
+		int i = -10;
+		if (i < 0) {
+			// if negative value. cnvert into positive value
+			i = -i;
+		}
+		System.out.println("the value of i is " + i);
+		// at this point the assumption is that i cannot be negative.
+		assert (i >= 0) : "impossible: i is negative. ";
+	}
+}
