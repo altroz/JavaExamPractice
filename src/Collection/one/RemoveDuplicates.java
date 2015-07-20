2033091a43a60de3d9d9d75f05a830647e2cedec
altroz <nhb913@outlook.com>
2015-07-20 10:18:02 -0700

    RemoveDuplicates.java class

---
+++ src/Collection/one/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+package Collection.one;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+	public static void main(String[] args) {
+		String tongueTwister = "I feel, a feel, a funny feel, a funny feel I feel";
+		Set<String> words = new HashSet<String>();
+
+		for (String word : tongueTwister.split("\\W++"))
+			words.add(word);
+		System.out.println("The tongue twister is: " + tongueTwister);
+		System.out.println("The words used were: ");
+		System.out.println(words);
+	}
+
+}
